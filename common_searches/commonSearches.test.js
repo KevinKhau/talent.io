ce2ca92459c5149a182825a41ee9f79ca70f7b9b
KevinKhau <kkhau@hotmail.fr>
2021-02-20 18:36:31 +0100

    TDD1 : setting unit tests

---
+++ common_searches/commonSearches.test.js
@@ -0,0 +1,37 @@
+const commonSearches = require('./commonSearches');
+
+const sampleSearches = require('./sampleSearches.json');
+const sampleRanking = require('./sampleRanking.json');
+const sampleRankingByCompany = require('./sampleRankingByCompany.json');
+
+const searches = require('./searches.json');
+const totalRanking = require('./total_ranking.json');
+const rankingByCompany = require('./ranking_by_company.json');
+
+test('givenSampleSearches_whenGetRanking_thenSampleRanking', () => {
+    const actual = commonSearches.getRanking(sampleSearches);
+    expect(actual).not.toBeNull();
+    expect(actual.length).toEqual(sampleRanking.length);
+    expect(actual).toStrictEqual(sampleRanking);
+});
+
+test('givenSamplesSearches_whenGetSampleRankingByCompany_thenSampleRankingByCompany', () => {
+    const actual = commonSearches.getRankingByCompany(sampleSearches);
+    expect(actual).not.toBeNull();
+    expect(actual.length).toEqual(sampleRankingByCompany.length);
+    expect(actual).toStrictEqual(sampleRankingByCompany);
+});
+
+test('givenSearches_whenGetRanking_thenTotalRanking', () => {
+    const actual = commonSearches.getRanking(searches);
+    expect(actual).not.toBeNull();
+    expect(actual.length).toEqual(totalRanking.length);
+    expect(actual).toStrictEqual(totalRanking);
+});
+
+test('givenSearches_whenGetRankingByCompany_thenRankingByCompany', () => {
+    const actual = commonSearches.getRankingByCompany(searches);
+    expect(actual).not.toBeNull();
+    expect(actual.length).toEqual(rankingByCompany.length);
+    expect(actual).toStrictEqual(rankingByCompany);
+});
