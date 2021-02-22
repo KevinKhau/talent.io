3d41100a7a72e613b0b0e45eac7b47e40390420c
KevinKhau <kkhau@hotmail.fr>
2021-02-23 00:32:42 +0100

    init coloured squares

---
+++ coloured_squares/src/App.test.js
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { container } = render(<App />);
+  const app = container.querySelector('.App')
+  expect(app).toBeInTheDocument();
+});
