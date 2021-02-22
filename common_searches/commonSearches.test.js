7698fee6f630985073c4df3bd9b3e5dcff8d2ee7
KevinKhau <kkhau@hotmail.fr>
2021-02-22 20:15:27 +0100

    TDD3: getRankingByCompany

--- common_searches/commonSearches.test.js
+++ common_searches/commonSearches.test.js
@@ -3,7 +3,8 @@ const commonSearches = require('./commonSearches');
 const sampleSearches = require('./sampleSearches.json');
 const sampleRanking = require('./sampleRanking.json');
 const sampleRankingByCompany = require('./sampleRankingByCompany.json');
-const sampleSearchGroupedBy = require('./sampleSearchesGroupedBy.json');
+const sampleSearchesGroupedBy = require('./sampleSearchesGroupedBy.json');
+const sampleSearchesGroupedByCompanyAndMore = require('./sampleSearchesGroupedByCompany+.json');
 
 const searches = require('./searches.json');
 const totalRanking = require('./total_ranking.json');
@@ -12,16 +13,22 @@ const rankingByCompany = require('./ranking_by_company.json');
 test('givenSampleSearches_whenGroupBy_thenSampleSearchesGroupedBy', () => {
     const actual = commonSearches.groupBy(sampleSearches);
     expect(actual).not.toBeNull();
-    expect(actual.length).toEqual(sampleSearchGroupedBy.length);
-    expect(actual).toStrictEqual(sampleSearchGroupedBy);
+    expect(actual.length).toEqual(sampleSearchesGroupedBy.length);
+    expect(actual).toStrictEqual(sampleSearchesGroupedBy);
 })
 
 test('givenSampleSearches_whenGetRanking_thenSampleRanking', () => {
     const actual = commonSearches.getRanking(sampleSearches);
-    console.log(actual);
     expect(actual).not.toBeNull();
     expect(actual.length).toEqual(sampleRanking.length);
     expect(actual).toStrictEqual(sampleRanking);
+});
+
+test('givenSampleSearches_whenGroupByCompany+_thenSampleSearchesGroupedByCompany+', () => {
+    const actual = commonSearches.groupBy(sampleSearches, ['company_id', 'technologies', 'positions']);
+    expect(actual).not.toBeNull();
+    expect(actual.length).toEqual(sampleSearchesGroupedByCompanyAndMore.length);
+    expect(actual).toStrictEqual(sampleSearchesGroupedByCompanyAndMore);
 });
 
 test('givenSamplesSearches_whenGetSampleRankingByCompany_thenSampleRankingByCompany', () => {
